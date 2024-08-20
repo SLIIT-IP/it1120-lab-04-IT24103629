c3e744eeb4f1ea8e198b340723cc3a739c33b2ec
VIRUL METHDINU MEEMANA <it24103629@my.sliit.lk>
2024-08-20 17:51:19 +0530

    Add files via upload

---
+++ Q2/IT24103629Lab4Q2.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class IT24103629Lab4Q2 {
+    public static void main(String[] args) {
+        Scanner mark = new Scanner(System.in);
+
+        double examMarks, labMarks, examPercentage, labPercentage, finalMarks;
+		
+        System.out.print("Please enter exam marks (out of 100): ");
+        examMarks = mark.nextDouble();
+        if (examMarks < 0 || examMarks > 100) {
+            System.out.println("Invalid input for exam marks. Terminating program.");
+            return; 
+        }
+
+        
+        System.out.print("Please enter lab marks (out of 100): ");
+        labMarks = mark.nextDouble();
+        if (labMarks < 0 || labMarks > 100) {
+            System.out.println("Invalid input for lab marks. Terminating program.");
+            return; 
+        }
+
+        
+        System.out.print("Please enter the percentage given for the exam: ");
+        examPercentage = mark.nextDouble();
+        if (examPercentage < 0 || examPercentage > 100) {
+            System.out.println("Invalid input for the percentage given for the exam. Terminating program.");
+            return; 
+        }
+
+        
+        System.out.print("Please enter the percentage given for the lab: ");
+        labPercentage = mark.nextDouble();
+        if (labPercentage < 0 || labPercentage > 100) {
+            System.out.println("Invalid input for the percentage given for the lab submission. Terminating program.");
+            return; 
+        }
+
+        
+        if (examPercentage + labPercentage != 100) {
+            System.out.println("The percentages must add up to 100. Terminating program.");
+            return;
+        }
+
+        finalMarks = (examMarks * examPercentage / 100) + (labMarks * labPercentage / 100);
+		
+        System.out.println("Final exam mark is: " + finalMarks);
+    }
+}
